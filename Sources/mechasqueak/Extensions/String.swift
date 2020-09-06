f8409d7c1254c3f69e4028fc201fd94b6839b0e9
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-09-06 23:25:42 +0200

    Remove duplicate random extension

--- Sources/mechasqueak/Extensions/String.swift
+++ Sources/mechasqueak/Extensions/String.swift
@@ -25,19 +25,6 @@
 import Foundation
 
 extension String {
-    static func random (length: Int = 20) -> String {
-        let base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        var randomString: String = ""
-
-        for _ in 0..<length {
-            let randomValue = arc4random_uniform(UInt32(base.count))
-            randomString += "\(base[base.index(base.startIndex, offsetBy: Int(randomValue))])"
-        }
-        return randomString
-    }
-}
-
-extension String {
     subscript (index: Int) -> Character {
         return self[self.index(self.startIndex, offsetBy: index)]
     }
