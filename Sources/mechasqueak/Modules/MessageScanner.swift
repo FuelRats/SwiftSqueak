e93b7142ec78a6e2cbe50c28f7dae7a7bc2a0eab
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-09-15 18:51:18 +0200

    Add even more case relevant phrases

--- Sources/mechasqueak/Modules/MessageScanner.swift
+++ Sources/mechasqueak/Modules/MessageScanner.swift
@@ -38,7 +38,8 @@ class MessageScanner: IRCBotModule {
 
     static let caseRelevantPhrases = [
         "fr+", "fr-", "wr+", "wr-", "bc+", "bc-", "fuel+", "fuel-", "sys-", "sysconf", "destroyed", "exploded",
-        "code red", "oxygen", "supercruise", "prep-", "prep+", "ez", "inst-", "open", "menu", "private", "actual"
+        "code red", "oxygen", "supercruise", "prep-", "prep+", "ez", "inst-", "open", "menu", "private", "actual",
+        "solo", "ready", "pos+"
     ]
 
     required init(_ moduleManager: IRCBotModuleManager) {
