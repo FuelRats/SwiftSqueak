5b3665fd3c7ab4a870b3aa1dac4381161ca10a49
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-10-31 00:33:37 +0100

    Improve detection of incomplete star systems

--- Sources/mechasqueak/SystemsAPI/SystemsAPI.swift
+++ Sources/mechasqueak/SystemsAPI/SystemsAPI.swift
@@ -114,7 +114,7 @@ class SystemsAPI {
                 return
             }
 
-            guard system.lowercased().hasSuffix("sector") == false else {
+            guard system.lowercased().hasSuffix("sector") == false && sectors.contains(where: { $0.name == system.uppercased() }) == false else {
                 mecha.reportingChannel?.send(key: "board.signal.unhelpfulsysmeme", map: [
                     "system": system
                 ])
