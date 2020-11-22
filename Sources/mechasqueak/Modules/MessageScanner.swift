0f394c0c1da3a26e528828bb1499066ef9ebd5f0
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-11-22 15:12:44 +0100

    Add another "stand down" variation to case relevant phrases

--- Sources/mechasqueak/Modules/MessageScanner.swift
+++ Sources/mechasqueak/Modules/MessageScanner.swift
@@ -39,7 +39,7 @@ class MessageScanner: IRCBotModule {
     static let caseRelevantPhrases = [
         "fr+", "fr-", "wr+", "wr-", "bc+", "bc-", "fuel+", "fuel-", "sys-", "sysconf", "destroyed", "exploded",
         "code red", "oxygen", "supercruise", "prep-", "prep+", "ez", "inst-", "open", "menu", "private", "actual",
-        "solo", "ready", "pos+", "rdy", "stand down", "stnd", "log off", "mmconf", "sys", "system"
+        "solo", "ready", "pos+", "rdy", "stand down", "stnd", "stnd", "log off", "mmconf", "sys", "system"
     ]
 
     required init(_ moduleManager: IRCBotModuleManager) {
