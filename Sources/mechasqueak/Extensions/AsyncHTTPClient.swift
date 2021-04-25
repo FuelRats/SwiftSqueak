dc78848b5a87529e8a6b5e49cba6a047f2a5e074
Alex S. Glomsaas <alex@sorlie.io>
2021-04-25 21:46:43 +0200

    Remove accidental debug statement

--- Sources/mechasqueak/Extensions/AsyncHTTPClient.swift
+++ Sources/mechasqueak/Extensions/AsyncHTTPClient.swift
@@ -47,7 +47,6 @@ extension HTTPClient {
                 case .success(let response):
 
                     do {
-                        try! decoder.decode(D.self, from: Data(buffer: response.body!))
                         let result = try decoder.decode(D.self, from: Data(buffer: response.body!))
                         promise.succeed(result)
                     } catch {
