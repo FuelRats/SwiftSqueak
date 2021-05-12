d8ebde639049fcc0f95e63d34770a658a9501024
Alex S. Glomsaas <alex@sorlie.io>
2021-05-13 01:37:11 +0200

    Don't reopen recent case if it closed with a fuel+

--- Sources/mechasqueak/RescueBoard/RescueBoard.swift
+++ Sources/mechasqueak/RescueBoard/RescueBoard.swift
@@ -198,36 +198,37 @@ class RescueBoard {
             return
         }
         
-        if let recentRescue = recentlyClosed.first(where: {
+        if let (_, recentRescue) = recentlyClosed.first(where: {
             $0.value.client == rescue.client && Date().timeIntervalSince($0.value.updatedAt) < 900
         }), configuration.general.drillMode == false, initiated != .insertion {
-            FuelRatsAPI.getRescue(id: recentRescue.value.id, complete: { result in
-                let apiRescue = result.body.data!.primary.value
-                let rats = result.assignedRats()
-                let firstLimpet = result.firstLimpet()
+            if recentRescue.quotes.contains(where: { $0.message.contains("fuel+") }) == false {
+                FuelRatsAPI.getRescue(id: recentRescue.id, complete: { result in
+                    let apiRescue = result.body.data!.primary.value
+                    let rats = result.assignedRats()
+                    let firstLimpet = result.firstLimpet()
 
-                let rescue = LocalRescue(
-                    fromAPIRescue: apiRescue,
-                    withRats: rats,
-                    firstLimpet: firstLimpet,
-                    onBoard: mecha.rescueBoard
-                )
-                if rescue.hasConflictingId(inBoard: mecha.rescueBoard) {
-                    rescue.commandIdentifier = mecha.rescueBoard.getNewIdentifier()
-                }
-                rescue.outcome = nil
-                rescue.status = .Open
+                    let rescue = LocalRescue(
+                        fromAPIRescue: apiRescue,
+                        withRats: rats,
+                        firstLimpet: firstLimpet,
+                        onBoard: mecha.rescueBoard
+                    )
+                    if rescue.hasConflictingId(inBoard: mecha.rescueBoard) {
+                        rescue.commandIdentifier = mecha.rescueBoard.getNewIdentifier()
+                    }
+                    rescue.outcome = nil
+                    rescue.status = .Open
 
-                mecha.rescueBoard.rescues.append(rescue)
-                rescue.syncUpstream()
-                message.reply(message: lingo.localize("rescue.reopen.opened", locale: "en-GB", interpolations: [
-                    "id": recentRescue.value.id.ircRepresentation,
-                    "caseId": rescue.commandIdentifier
-                ]))
-            }, error: { _ in
-            })
-            
-            return
+                    mecha.rescueBoard.rescues.append(rescue)
+                    rescue.syncUpstream()
+                    message.reply(message: lingo.localize("rescue.reopen.opened", locale: "en-GB", interpolations: [
+                        "id": recentRescue.id.ircRepresentation,
+                        "caseId": rescue.commandIdentifier
+                    ]))
+                }, error: { _ in
+                })
+                return
+            }
         }
         
         var even: Bool? = nil
