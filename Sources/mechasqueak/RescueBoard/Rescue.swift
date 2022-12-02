9d1096c52a6bde4e64e94a916180627bb632f3ff
Alex Sørlie Glomsaas <alex@sorlie.io>
2022-12-02 12:13:34 -0500

    Automatically pull info from API when a manual signal is made by a registered rat

--- Sources/mechasqueak/RescueBoard/Rescue.swift
+++ Sources/mechasqueak/RescueBoard/Rescue.swift
@@ -168,6 +168,18 @@ class Rescue {
         self.createdAt = Date()
         self.updatedAt = Date()
         self.uploaded = false
+        
+        if message.user.associatedAPIData != nil {
+            if let providedPlatform = GamePlatform.parsedFromText(text: signal.platform ?? ""), let rat = message.user.getRatRepresenting(platform: providedPlatform) {
+                self.client = rat.name
+                self.platform = rat.platform
+                self.expansion = rat.expansion
+            } else if let rat = message.user.currentRat {
+                self.client = rat.name
+                self.platform = rat.platform
+                self.expansion = rat.expansion
+            }
+        }
     }
 
     init? (text: String, clientName: String, fromCommand command: IRCBotCommand) {
